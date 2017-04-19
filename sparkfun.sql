51b6902fe764f5737de16abba0851d2b6af8db3e
Marco <marco.mangan@gmail.com>
2017-04-18 22:39:19 -0300

    Update sparkfun.sql

--- sparkfun.sql
+++ sparkfun.sql
@@ -1,4 +1,4 @@
-create table products(
+create table app.products(
 id integer not null primary key generated always as identity(start with 1, increment by 1),
 name varchar(30) not null,
 description varchar(300) not null,
