796b055ca7b91e67101c8c984704302de514ea0b
Marco <marco.mangan@gmail.com>
2017-04-25 21:30:49 -0300

    Update sparkfun.sql

--- sparkfun.sql
+++ sparkfun.sql
@@ -1,7 +1,14 @@
+--
+-- marco.mangan@pucrs.br
+--
+drop table app.products;
+
 create table app.products(
 id integer not null primary key generated always as identity(start with 1, increment by 1),
 name varchar(30) not null,
 description varchar(300) not null,
 image varchar(300) not null,
 value decimal (10,2)
-)
+);
+
+insert into app.products(name, description, image, value) values ('teste1', '"teste2', 'teste3', 12.34);
